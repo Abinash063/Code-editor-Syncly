0a9fd1690d4dcbe502d81a8cc6097ac2832567c5
Abinash063 <pandababinash@gmail.com>
2025-08-23 10:58:49 +0530

    deployment fixes

--- Backend/index.js
+++ Backend/index.js
@@ -88,11 +88,11 @@ io.on("connection", (socket) => {
 const port = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+//app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
+//app.use((req, res) => {
+//  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+//});
 
 
 server.listen(port, () => {
